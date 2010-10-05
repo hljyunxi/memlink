667fb8c8b8cfddca05d34bce879131f11e09a5e0
zhaoweikid <zhaoweikid@8774d0fb-9826-d56a-0b93-cb074e894c85>
2010-10-05 06:02:14 +0000

    add memlink php client

--- trunk/client/php/test.php
+++ trunk/client/php/test.php
@@ -1,10 +1,23 @@
 <?
-include_once("memlink.php");
+include_once("memlinkclient.php");
 
-$conn = memlink_create("127.0.0.1", 11001, 11002, 10);
+$class = new ReflectionClass("MemLink");
+$methods = $class->getMethods();
 
-memlink_destroy($conn);
+foreach ($methods as $method) {
+    echo "method:".$method->getName()."\n";
+}
 
+
+$m = new MemLinkClient("127.0.0.1", 11001, 11002, 10);
+echo "host:".$m->readport;
+$key = "haha1";
+
+$ret = $m->stat($key);
+
+$ret = $m->range($key, "::", 3, 10);
+
+$m->destroy();
 
 ?>
 
