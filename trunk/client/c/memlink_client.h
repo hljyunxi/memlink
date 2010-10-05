54fa4607bb284330bf77c8ba0fe64337b641166c
zhaoweikid <zhaoweikid@8774d0fb-9826-d56a-0b93-cb074e894c85>
2010-10-05 02:08:23 +0000

    add memlink cliet for python

--- trunk/client/c/memlink_client.h
+++ trunk/client/c/memlink_client.h
@@ -30,6 +30,8 @@
 #define MEMLINK_ERR_EKEY			-15
 // 网络错误
 #define MEMLINK_ERR_CONNECT			-16
+// 返回代码错误
+#define MEMLINK_ERR_RETCODE         -17
 // 其他错误
 #define MEMLINK_ERR					-1
 // 执行成功
@@ -45,20 +47,53 @@ typedef struct _memlink_client
     int     timeout;
 }MemLink;
 
+typedef struct _memlink_stat
+{
+    unsigned char   valuesize;
+    unsigned char   masksize;
+    unsigned int    blocks; // all blocks
+    unsigned int    data;   // all alloc data item
+    unsigned int    data_used; // all data item used
+    unsigned int    mem;       // all alloc mem
+    unsigned int    mem_used;  // all used mem
+}MemLinkStat;
+
+typedef struct _memlink_item
+{
+    struct _memlink_item    *next;
+    char                    value[256];
+    char                    mask[256];
+}MemLinkItem;
+
+typedef struct _memlink_result
+{
+    int             count;
+    int             valuesize;
+    int             masksize;
+    MemLinkItem     *root;
+}MemLinkResult;
+
+void        memlink_result_free(MemLinkResult *result);
+
 MemLink*    memlink_create(char *host, int readport, int writeport, int timeout);
 void        memlink_destroy(MemLink *m);
 void		memlink_close(MemLink *m);
 
 int         memlink_cmd_dump(MemLink *m);
 int			memlink_cmd_clean(MemLink *m, char *key);
-int			memlink_cmd_stat(MemLink *m, char *key);
+int			memlink_cmd_stat(MemLink *m, char *key, MemLinkStat *stat);
 int			memlink_cmd_create(MemLink *m, char *key, int valuelen, char *maskstr);
 int			memlink_cmd_del(MemLink *m, char *key, char *value, int valuelen);
-int			memlink_cmd_insert(MemLink *m, char *key, char *value, int valuelen, char *maskstr, unsigned int pos);
-int			memlink_cmd_update(MemLink *m, char *key, char *value, int valuelen, unsigned int pos);
-int			memlink_cmd_mask(MemLink *m, char *key, char *value, int valuelen, char *maskstr);
+int			memlink_cmd_insert(MemLink *m, char *key, char *value, int valuelen, 
+                               char *maskstr, unsigned int pos);
+int			memlink_cmd_update(MemLink *m, char *key, char *value, int valuelen, 
+                               unsigned int pos);
+int			memlink_cmd_mask(MemLink *m, char *key, char *value, int valuelen, 
+                             char *maskstr);
 int			memlink_cmd_tag(MemLink *m, char *key, char *value, int valuelen, int tag);
-int			memlink_cmd_range(MemLink *m, char *key, char *maskstr, unsigned int frompos, unsigned int len);
+int			memlink_cmd_range(MemLink *m, char *key, char *maskstr, 
+                              unsigned int frompos, unsigned int len,
+                              MemLinkResult *result);
 
 #endif
 
