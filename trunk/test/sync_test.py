c4fa60a920e5af4d39fc2244096059d09bcef74c
yaojingguo <yaojingguo@8774d0fb-9826-d56a-0b93-cb074e894c85>
2010-10-20 03:09:13 +0000

    Add sync dump command

--- trunk/test/sync_test.py
+++ trunk/test/sync_test.py
@@ -1,25 +1,43 @@
 from socket import *
 from struct import *
 
-def test(sock, log_ver, log_index):
-  sock.send(pack('H', 9))
-  sock.send(pack('B', 100))
-  sock.send(pack('II', log_ver, log_index))
+def cmd(sock, data):
+  for param in data:
+    sock.send(param)
   response = sock.recv(1024)
   print(repr(response))
+
+
+def cmd_sync(sock, log_ver, log_index):
+  data = [pack('H', 9),
+          pack('B', 100),
+          pack('II', log_ver, log_index)]
+  cmd(sock, data)
+
+def cmd_sync_dump(sock, dumpver, size):
+  data = [pack('H', 13),
+          pack('B', 101),
+          pack('I', dumpver),
+          pack('Q', size)]
+  cmd(sock, data)
+
+def test_cmd_sync(sock):
+  # valid
+  cmd_sync(sock, 1, 4)
+  # invalid index 
+  cmd_sync(sock, 1, 4000)
+  # invalid log version
+  cmd_sync(sock, 2, 4)
+
+def test_cmd_sync_dump(sock):
+  cmd_sync_dump(sock, 1, 0)
 
 def main():
   sock = socket(AF_INET, SOCK_STREAM);
   sock.connect(('localhost', 11003));
 
-  # valid
-  test(sock, 1, 4)
-
-  # invalid index 
-  test(sock, 1, 4000)
-
-  # invalid log version
-  test(sock, 2, 4)
+  test_cmd_sync(sock)
+  test_cmd_sync_dump(sock)
 
   sock.close()
 
