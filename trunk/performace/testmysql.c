7929cf6945bb9c5cc2a6f6a0e4815b5134c7d905
zhaoweikid <zhaoweikid@8774d0fb-9826-d56a-0b93-cb074e894c85>
2010-11-10 11:25:13 +0000

    change mysql test

--- trunk/performace/testmysql.c
+++ trunk/performace/testmysql.c
@@ -6,7 +6,7 @@
 
 #define MYSQL_HOST	"127.0.0.1"
 #define MYSQL_USER	"root"
-#define MYSQL_PASS	""
+#define MYSQL_PASS	"123456"
 #define MYSQL_DB	"mytest"
 
 
@@ -77,9 +77,7 @@ int test_insert_long(int count, int docreate)
 
 	gettimeofday(&start, NULL);	
 	for (i = 0; i < count; i++) {
-		//sprintf(val, "%012d", i);
 		sqllen = snprintf(sql, 1024, sql_format, i);
-		//DINFO("lpush %s, %s\n", key, val);
 		ret = mysql_real_query(&mysql, sql, sqllen);
 		if (ret != 0) {
 			DERROR("mysql query error: %s\n", mysql_error(&mysql));
@@ -101,6 +99,46 @@ int test_insert_long(int count, int docreate)
 
 int test_insert_short(int count, int docreate)
 {
+	MYSQL	mysql;
+	struct timeval start, end;
+
+	int  i;
+	int  ret;
+	char *key = "haha";
+	char val[32];
+	char sql[1024];
+	int  sqllen;
+	//char sql_format
+
+	char *sql_format = "insert into ThreadList values (1, '%12d', 0, now())";
+
+	gettimeofday(&start, NULL);	
+
+	for (i = 0; i < count; i++) {
+		mysql_init(&mysql);
+
+		if (!mysql_real_connect(&mysql, MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB, 0, NULL, 0)) {
+			DERROR("mysql connect error! %s\n", mysql_error(&mysql));
+			return -1;
+		}
+		sqllen = snprintf(sql, 1024, sql_format, i);
+		ret = mysql_real_query(&mysql, sql, sqllen);
+		if (ret != 0) {
+			DERROR("mysql query error: %s\n", mysql_error(&mysql));
+			return -2;
+		}
+
+		mysql_close(&mysql);
+	}
+	gettimeofday(&end, NULL);	
+
+	unsigned int tmd = timediff(&start, &end);
+	double speed = ((double)count / tmd) * 1000000;
+	DINFO("insert long use time: %u, speed: %.2f\n", tmd, speed);
+	
+
+
+	return (int)speed;
 
 	return 0;
 }
@@ -142,7 +180,7 @@ int test_range_long(int frompos, int slen, int count)
 			DERROR("mysql result error: %s\n", mysql_error(&mysql));
 			return -2;
 		}
-		if ((ret = mysql_num_rows(&mysql)) != slen) {
+		if ((ret = mysql_num_rows(result)) != slen) {
 			DERROR("mysql query count error: %d\n", ret);
 			return -2;
 		}
@@ -161,7 +199,57 @@ int test_range_long(int frompos, int slen, int count)
 
 int test_range_short(int frompos, int slen, int count)
 {
-	return 0;
+	MYSQL		mysql;
+	MYSQL_RES	*result;
+	struct timeval start, end;
+	int  i;
+	int  ret;
+	char *key = "haha";
+	char val[32];
+	char sql[1024];
+	int  sqllen;
+	//char sql_format
+
+	char *sql_format = "select threadid from  ThreadList where forumid=1 order by reply_time limit %d,%d";
+
+	gettimeofday(&start, NULL);	
+
+	for (i = 0; i < count; i++) {
+		mysql_init(&mysql);
+
+		if (!mysql_real_connect(&mysql, MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB, 0, NULL, 0)) {
+			DERROR("mysql connect error! %s\n", mysql_error(&mysql));
+			return -1;
+		}
+
+		sqllen = snprintf(sql, 1024, sql_format, frompos, slen);
+		//DINFO("sql: %s\n", sql);
+		ret = mysql_real_query(&mysql, sql, sqllen);
+		if (ret != 0) {
+			DERROR("mysql query error: %s\n", mysql_error(&mysql));
+			return -2;
+		}
+		result = mysql_store_result(&mysql);
+		if (result == NULL) {
+			DERROR("mysql result error: %s\n", mysql_error(&mysql));
+			return -2;
+		}
+		if ((ret = mysql_num_rows(result)) != slen) {
+			DERROR("mysql query count error: %d\n", ret);
+			return -2;
+		}
+		mysql_free_result(result);
+
+		mysql_close(&mysql);
+	}
+	gettimeofday(&end, NULL);	
+
+	unsigned int tmd = timediff(&start, &end);
+	double speed = ((double)count / tmd) * 1000000;
+	DINFO("range long use time: %u, speed: %.2f\n", tmd, speed);
+	
+
+	return (int)speed;
 }
 
 
