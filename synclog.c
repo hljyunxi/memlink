32cacecaa1ff6474922e6f30e35d5442f4ab6b0c
zhaoweikid <zhaoweikid@8774d0fb-9826-d56a-0b93-cb074e894c85>
2010-11-11 03:10:00 +0000

    change

--- synclog.c
+++ synclog.c
@@ -214,6 +214,8 @@ synclog_new(SyncLog *slog)
         return -1;
     }
 
+	g_runtime->logver += 1;
+
     unsigned short format = DUMP_FORMAT_VERSION;
     unsigned int   newver = g_runtime->logver;
     unsigned int   synlen = SYNCLOG_INDEXNUM;
@@ -247,8 +249,6 @@ synclog_new(SyncLog *slog)
 
     slog->pos       = slog->len;
     slog->index_pos = 0;
-    
-	g_runtime->logver += 1;
 
     return 0;
 }
