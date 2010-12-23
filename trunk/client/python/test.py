5cf79b200e24f112c97738443e7af16ed7fd243c
zhaoweikid <zhaoweikid@8774d0fb-9826-d56a-0b93-cb074e894c85>
2010-12-23 06:59:56 +0000

    change test

--- trunk/client/python/test.py
+++ trunk/client/python/test.py
@@ -6,6 +6,8 @@ from memlinkclient import *
 READ_PORT  = 11001
 WRITE_PORT = 11002
 
+key = 'haha'
+
 def insert(*args):
     try:
         start = int(args[0])
@@ -13,30 +15,50 @@ def insert(*args):
     except:
         start = 0
         num   = 1000
-    print 'insert:', start, num
+
+    try:
+        val = '%012d' % int(args[2])
+    except:
+        val = None
+
+    print 'insert:', start, num, val
 
     m = MemLinkClient('127.0.0.1', READ_PORT, WRITE_PORT, 10)
    
-    ret = m.create('haha', 12, "1")
+    ret = m.create(key, 12, "1")
     if ret != MEMLINK_OK:
         print 'create haha error!', ret
         #return
 
     for i in xrange(start, start + num):
-        print 'insert:', '%012d' % i
-        ret = m.insert('haha', '%012d' % i, "1", 0)
+        if not val:
+            val2 = '%012d' % i
+        else:
+            val2 = val
+        print 'insert:', val2
+        ret = m.insert(key, val2, "1", i)
         if ret != MEMLINK_OK:
             print 'insert error:', ret, i
             return
 
     m.destroy()
 
-def delete():
+def delete(*args):
     m = MemLinkClient('127.0.0.1', READ_PORT, WRITE_PORT, 10)
+    val = '%012d' % int(args[0]) 
+    print 'delete:', val
+    ret = m.delete(key, val)
+    if ret != MEMLINK_OK:
+        print 'delete error:', ret, val
+        return
+
+    m.destroy()
+    
+    return
 
     for i in xrange(1, 300):
         print 'del %012d' % (i*2)
-        ret = m.delete('haha', '%012d' % (i*2))
+        ret = m.delete(key, '%012d' % (i*2))
         if ret != MEMLINK_OK:
             print 'delete error:', ret, i*2
             return
@@ -58,7 +80,7 @@ def range(*args):
 
     m = MemLinkClient('127.0.0.1', READ_PORT, WRITE_PORT, 10)
     
-    ret, recs = m.range('haha', mask, frompos, slen)
+    ret, recs = m.range(key, mask, frompos, slen)
     if ret != MEMLINK_OK:
         print 'range error:', ret
         return
@@ -81,19 +103,36 @@ def dump(*args):
 
 def clean(*args):
     m = MemLinkClient('127.0.0.1', READ_PORT, WRITE_PORT, 10)
-    ret = m.clean('haha')
+    ret = m.clean(key)
     if ret != MEMLINK_OK:
         print 'clean error!', ret
     m.destroy()
 
 def stat(*args):
     m = MemLinkClient('127.0.0.1', READ_PORT, WRITE_PORT, 10)
-    ret, stat = m.stat('haha')
+    ret, stat = m.stat(key)
     if ret != MEMLINK_OK:
         print 'clean error!', ret
     print stat
     m.destroy()
 
+def ping(*args):
+    try:
+        count = int(args[1])
+    except:
+        count = 1000
+    print 'ping count:', count
+    start = time.time()
+    m = MemLinkClient('127.0.0.1', READ_PORT, WRITE_PORT, 10)
+    for i in xrange(0, count):
+        ret = m.ping()
+        if ret != MEMLINK_OK:
+            print 'ping error:', ret
+            return 
+    m.destroy()
+    end = time.time()
+
+    print 'use time:', end - start, 'speed:', count / (end-start)
 
 if __name__ == '__main__':
     action = sys.argv[1]
@@ -103,6 +142,6 @@ if __name__ == '__main__':
 
     func = globals()[action]
     func(*args)
-        
+            
 
 
