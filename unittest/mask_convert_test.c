0bedc33851172a2b2ba55743869aa0273e70f554
zhaoweikid <zhaoweikid@8774d0fb-9826-d56a-0b93-cb074e894c85>
2011-03-04 10:51:48 +0000

    change test

--- unittest/mask_convert_test.c
+++ unittest/mask_convert_test.c
@@ -9,18 +9,15 @@
 int mask_string2array_test()//随机生成一个mask，0-20个项，每项的值0-256
 {
 	int num = 1 + my_rand(HASHTABLE_MASK_MAX_ITEM);
-	//num = 1;
 	char mask[512] = {0};
 	char buf[10];
 	int i = 1;
-	//for(i = 0; i < num; i++)
-	//printf("num:%d\n", num);
-	while(i <= num)
+	
+    while(i <= num)
 	{
 		int val = my_rand(512);
 		sprintf(buf, "%d", val);
-		do
-		{
+		do {
 			if(val > 256 && num != 1)
 				break;			
 			strcat(mask, buf);
@@ -30,8 +27,7 @@ int mask_string2array_test()//随机生成一个mask，0-20个项，每项的值
 			strcat(mask, ":");
 		i++;
 	}
-	//printf("mask=%s\n", mask);
-	int result[128];
+	unsigned int result[128];
 	int ret = mask_string2array(mask, result);
 
 	if(ret != num)
@@ -46,32 +42,18 @@ int mask_string2array_test()//随机生成一个mask，0-20个项，每项的值
 
 int mask_string2binary_binary2string()
 {
-	/*
-	char mask2[512] = {0};
-	char maskformatnum2[3] = {4, 2, 3};
-	char maskstr2[512] = {0};
-	strcpy(maskstr2, "8:1:4");
-	int ret2 = mask_string2binary(maskformatnum2, maskstr2, mask2);
-	char buf3[128];
-	DINFO("mask:%s\n", formath(mask2, 2, buf3, 128));
-	char maskstr3[512] = {0};
-	mask_binary2string(maskformatnum2, 3, mask2, 2, maskstr3);
-	printf("maskstr3 = %s\n", maskstr3);
-	return 0;
-	*/
-	int i = 0;int j = 0;
-	for (j = 0; j < 50; j++)
-	{
+	int i = 0;
+    int j = 0;
+
+	for (j = 0; j < 50; j++) {
 		int num = 1 + my_rand(5);
-		//num = 5;
 		char maskformat[512] = {0};
-		char maskformatnum[100] = {0};
+		unsigned char maskformatnum[100] = {0};
 		char maskstr[512] = {0};
 		DINFO("num:%d\n", num);		
 		int len = 2;
 
-		for(i = 1; i <= num; i++)
-		{
+		for(i = 1; i <= num; i++) {
 			//随机生成maskformat
 			char buf[10] = {0};
 			char buf2[10] = {0};
@@ -86,8 +68,7 @@ int mask_string2binary_binary2string()
 			int max = pow(2, val) - 1;
 			int k= 1 + my_rand(max);
 			sprintf(buf2, "%d", k);
-			do
-			{
+			do {
 				if(k > 1024)
 					break;			
 				strcat(maskstr, buf2);
@@ -110,147 +91,181 @@ int mask_string2binary_binary2string()
 		//DINFO("maskstr1=%s\n\n", maskstr1);		
 		DINFO("maskformat=%s, maskstr=%s, maskstr1=%s\n", maskformat, maskstr, maskstr1);
 
-		if(0 != strcmp(maskstr, maskstr1))
-		{	
+		if(0 != strcmp(maskstr, maskstr1)) {	
 			DERROR("================ERROR!=============== ");
 			DINFO("mask:%s\n", formath(mask, masklen, buf2, 128));
 			return -1;
-			//DERROR("maskstr1=%s, maskstr=%s\n", maskstr1, maskstr);
 		}
 	}
 	return 0;
 }
 
-int mask_array2binary_test()
+
+int array2bin_test_one(unsigned char *format, unsigned int *array, char num, unsigned char *mask, unsigned char msize)
 {
 	int ret;
-    unsigned char format1[3] = {4, 3, 1};
-    unsigned int  maskarray1[3] = {UINT_MAX, 2, 0};
-	//00 10000001
-	char mask1[2] = {0x81, 0};
-    unsigned int  masknum1 = 3;
     char data[1024] = {0};
-    ret = mask_array2binary(format1, maskarray1, masknum1, data);
-	if(ret != 2)
-	{
-		DERROR("mask_array2binary error. ret:%d\n", ret);
+
+    ret = mask_array2binary(format, array, num, data);
+	if (ret != msize) {
+		DERROR("mask_array2binary error. ret:%d, mask size:%d\n", ret, msize);
 		return -1;
-	}
-	else
-	{
-		ret = memcmp(data, mask1, 2);
-		if(0 != ret)
-		{
-			DERROR("mask1 memcmp error. \n");
+	}else{
+		ret = memcmp(data, mask, msize);
+		if (0 != ret) {
+            char buf[512] = {0};
+			DERROR("mask memcmp error. %s\n", formath(data, msize, buf, 512));
 			return -1;
-		}
-		else
+		}else{
 			DINFO("test1 ok!\n");
-	}
-    unsigned char format2[3] = {4, 3, 1};
-    unsigned int  maskarray2[3] = {5, 2, 2};
-	//00 10010101
-	char mask2[2] = {0x95, 0}; 
-    unsigned int  masknum2 = 3;
-	memset(data, 0, 1024);	
-    ret = mask_array2binary(format2, maskarray2, masknum2, data);
-	if(ret != 2)
-	{
-		DERROR("mask_array2binary error. ret:%d\n", ret);
-		return -1;
-	}
-	else
-	{
-		ret = memcmp(data, mask2, 2);
-		if(0 != ret)
-		{
-			DERROR("mask2 memcmp error. \n");
-			return -1;
-		}
-		else
-			DINFO("test2 ok!\n");
+        }
 	}
 
-    unsigned char format3[3] = {4, 3, 8};
-    unsigned int  maskarray3[3] = {5, 2, 128};
-	// 1 00000000 10010101
-	char mask3[3] = {0x95, 0, 0x01}; 
-    unsigned int  masknum3 = 3;
-	memset(data, 0, 1024);	
-    ret = mask_array2binary(format3, maskarray3, masknum3, data);
-	if(ret != 3)
-	{
-		DERROR("mask_array2binary error. ret:%d\n", ret);
-		return -1;
-	}
-	else
-	{
-		ret = memcmp(data, mask3, 3);
-		if(0 != ret)
-		{
-			DERROR("mask3 memcmp error. \n");
-			return -1;
-		}
-		else
-			DINFO("test3 ok!\n");
-	}
+    return 0;
+}
 
-    unsigned char format4[2] = {32, 1};
-    unsigned int  maskarray4[2] = {0x23, 0x01};
-	// 0 00000001 1111 1101
-	char mask4[5] = {0x8d, 0, 0, 0, 0x04}; 
-    unsigned int  masknum4 = 2;
-	/*memset(data, 0, 1024);	
-	int val = 0x03fd;
-	memcpy(data, &val, 4);
-	printb(data, 17);
-	printf("==========================\n");*/
-	memset(data, 0, 1024);	
-    ret = mask_array2binary(format4, maskarray4, masknum4, data);
-	if(ret != 5)
-	{
-		DERROR("mask_array2binary error. ret:%d\n", ret);
-		return -1;
-	} else {
-        char buf[512];
-    	DINFO("%s\n", formatb(data, ret, buf, 512));
-		ret = memcmp(data, mask4, 5);
-		if(0 != ret)
-		{
-			DERROR("mask4 memcmp error. \n");
-			return 0;
-		}
-		else
-			DINFO("test4 ok!\n");
-	}
+typedef struct testitem
+{
+    unsigned char format[16];
+    unsigned int  array[16];
+    int           num;
+    unsigned char mask[512];
+    int           msize;
+}TestItem;
+
+int mask_array2binary_test()
+{
+    TestItem    testitems[7];
+    
+    TestItem    *item = &testitems[0];
+    item->num       = 3;
+    item->format[0] = 4;
+    item->format[1] = 3;
+    item->format[2] = 1;
+    item->array[0]  = UINT_MAX;
+    item->array[1]  = 2;
+    item->array[2]  = 0;
+    item->mask[0]   = 0x81;
+    item->mask[1]   = 0;
+    item->msize     = 2;
+
+    item = &testitems[1];
+    item->num       = 3;
+    item->format[0] = 4;
+    item->format[1] = 3;
+    item->format[2] = 1;
+    item->array[0]  = 5;
+    item->array[1]  = 2;
+    item->array[2]  = 2;
+    item->mask[0]   = 0x95;
+    item->mask[1]   = 0;
+    item->msize     = 2;
+
+    item = &testitems[2];
+    item->num       = 3;
+    item->format[0] = 4;
+    item->format[1] = 3;
+    item->format[2] = 8;
+    item->array[0]  = 5;
+    item->array[1]  = 2;
+    item->array[2]  = 128;
+    item->mask[0]   = 0x95;
+    item->mask[1]   = 0;
+    item->mask[2]   = 0x01;
+    item->msize     = 3;
+
+    item = &testitems[3];
+    item->num       = 2;
+    item->format[0] = 32;
+    item->format[1] = 1;
+    item->array[0]  = 0x23;
+    item->array[1]  = 0x01;
+    item->mask[0]   = 0x8d;
+    item->mask[1]   = 0;
+    item->mask[2]   = 0;
+    item->mask[3]   = 0;
+    item->mask[4]   = 0x04;
+    item->msize     = 5;
+
+    item = &testitems[4];
+    item->num       = 2;
+    item->format[0] = 16;
+    item->format[1] = 2;
+    item->array[0]  = 1;
+    item->array[1]  = 0x1111;
+    item->mask[0]   = 0x15;
+    item->mask[1]   = 0x11;
+    item->mask[2]   = 0x01;
+    item->msize     = 3;
+
+    item = &testitems[5];
+    item->num       = 2;
+    item->format[0] = 32;
+    item->format[1] = 2;
+    item->array[0]  = 1;
+    item->array[1]  = 0xffffffff;
+    item->mask[0]   = 0xf5;
+    item->mask[1]   = 0xff;
+    item->mask[2]   = 0xff;
+    item->mask[3]   = 0xff;
+    item->mask[4]   = 0x0f;
+    item->msize     = 5;
+
+    item = &testitems[6];
+    item->num       = 3;
+    item->format[0] = 32;
+    item->format[1] = 32;
+    item->format[2] = 2;
+    item->array[0]  = 1;
+    item->array[1]  = 0xfffffff;
+    item->array[2]  = 0x10101010;
+    item->mask[0]   = 0xf1;
+    item->mask[1]   = 0xff;
+    item->mask[2]   = 0xff;
+    item->mask[3]   = 0xff;
+    item->mask[4]   = 0x0f;
+    item->mask[5]   = 0x01;
+    item->mask[6]   = 0x01;
+    item->mask[7]   = 0x01;
+    item->mask[8]   = 0x01;
+    item->msize     = 9;
+
+    int i;
+    for (i = 0; i < 5; i++) {
+        item = &testitems[i];
+        int ret = array2bin_test_one(item->format, item->array, item->num, 
+                item->mask, item->msize);
+        if (ret != 0) {
+            DERROR("test error: %d, %d\n", i, ret);
+        }
+    }
 
 	return 0;
-	//printf("%x\n", UINT_MAX);
-    //printb(data, ret);
 }
 
 int main()
 {
-	//#define DEBUG
+	//logfile_create("test.log", 3);
+	logfile_create("stdout", 3);
 
-	logfile_create("test.log", 3);
-
-	int n = 0x03 & 0x02;
-	DINFO("-----------> %d\n", n);
 	int ret;
 	int i = 0;
-	for(i = 0; i < 10; i++)
-	{
+
+	for (i = 0; i < 10; i++) {
 		ret = mask_string2array_test();
 		if (0 != ret)
 			return -1;
 	}
 	
 	ret = mask_array2binary_test();
-	if (0 != ret)
+	if (0 != ret) {
 		return -1;
+    }
+
 	mask_string2binary_binary2string();
-	if (0 != ret)
+	if (0 != ret) {
 		return -1;
+    }
+
 	return 0;
 }
