9a00da1d5602d089e2447d9e0ab32937c8105479
zhaoweikid <zhaoweikid@8774d0fb-9826-d56a-0b93-cb074e894c85>
2011-08-22 10:22:39 +0000

    add table unittest

--- trunk/unittest/datablock_test.c
+++ trunk/unittest/datablock_test.c
@@ -18,92 +18,102 @@ int main()
     int  attrnum = 0; 
     int  i = 0, ret;
     unsigned int attrformat[4] = {0};
+    char *tbname = "test";
 
     sprintf(key, "haha%03d", i);
-    hashtable_key_create_attr(ht, key, valuesize, attrformat, attrnum, MEMLINK_LIST, 0);
+    ret = hashtable_create_table(ht, tbname, valuesize, attrformat, attrnum, MEMLINK_LIST, 0);
+    if (ret != MEMLINK_OK) {
+        DERROR("create table error:%s\n", tbname);
+        return -1;
+    }
+    Table *tb = hashtable_find_table(ht, tbname);
+    if (NULL == tb) {
+        DERROR("not found table:%s\n", tbname);
+        return -1;
+    }
 
     char attr[10] = {0x01};
     char value[16]= {0};
-    HashNode *node = hashtable_find(ht, key);
 
     for (i = 0; i < 1; i++) {
         sprintf(value, "%010d", i);
-        ret = hashtable_add_attr_bin(ht, key, value, attr, 0);
+        ret = hashtable_insert_binattr(ht, tbname, key, value, attr, 0);
         if (ret != MEMLINK_OK) {
-            DERROR("add error:%d, key:%s, value:%d\n", ret, key, i);
+            DERROR("insert error:%d, key:%s, value:%d\n", ret, key, i);
         }
     }
     DINFO("====== %d ======\n", i);
-    hashtable_print(ht, key);
+    HashNode *node = table_find(tb, key);
+    table_print(tb, key);
     assert(node->data->data_count == 1);
 
     for ( ; i < 2; i++) {
         sprintf(value, "%010d", i);
-        ret = hashtable_add_attr_bin(ht, key, value, attr, 0);
+        ret = hashtable_insert_binattr(ht, tbname, key, value, attr, 0);
         if (ret != MEMLINK_OK) {
             DERROR("add error:%d, key:%s, value:%d\n", ret, key, i);
         }
     }
     DINFO("====== %d ======\n", i);
-    hashtable_print(ht, key);
+    table_print(tb, key);
 
     assert(node->data->data_count == 2);
 
     for ( ; i < 5; i++) {
         sprintf(value, "%010d", i);
-        ret = hashtable_add_attr_bin(ht, key, value, attr, 0);
+        ret = hashtable_insert_binattr(ht, tbname, key, value, attr, 0);
         if (ret != MEMLINK_OK) {
             DERROR("add error:%d, key:%s, value:%d\n", ret, key, i);
         }
         DINFO("data count:%d, used:%d\n", node->data->data_count, node->used);
 
         DINFO("====== %d ======\n", i);
-        hashtable_print(ht, key);
+        table_print(tb, key);
 
         assert(node->data->data_count == 5);
     }
 
     for ( ; i < 10; i++) {
         sprintf(value, "%010d", i);
-        ret = hashtable_add_attr_bin(ht, key, value, attr, 0);
+        ret = hashtable_insert_binattr(ht, tbname, key, value, attr, 0);
         if (ret != MEMLINK_OK) {
             DERROR("add error:%d, key:%s, value:%d\n", ret, key, i);
         }
         DINFO("====== %d ======\n", i);
-        hashtable_print(ht, key);
+        table_print(tb, key);
 
         assert(node->data->data_count == 10);
     }
 
     for ( ; i < 23; i++) {
         sprintf(value, "%010d", i);
-        ret = hashtable_add_attr_bin(ht, key, value, attr, 0);
+        ret = hashtable_insert_binattr(ht, tbname, key, value, attr, 0);
         if (ret != MEMLINK_OK) {
             DERROR("add error:%d, key:%s, value:%d\n", ret, key, i);
         }
     }
     DINFO("====== %d ======\n", i);
-    hashtable_print(ht, key);
+    table_print(tb, key);
 
     assert(node->data->data_count == 5);
     
     for (i = 19; i >= 10; i--) {
         sprintf(value, "%010d", i);
-        ret = hashtable_del(ht, key, value);
+        ret = hashtable_del(ht, tbname, key, value);
         if (ret != MEMLINK_OK) {
             DERROR("del error: %d, key:%s,val:%s\n", ret, key, value); 
         }
-        hashtable_print(ht, key);
+        table_print(tb, key);
     }
 
     // insert
     for (i = 100; i < 110; i++) {
         sprintf(value, "%010d", i);
-        ret = hashtable_add_attr_bin(ht, key, value, attr, 0);
+        ret = hashtable_insert_binattr(ht, tbname, key, value, attr, 0);
         if (ret != MEMLINK_OK) {
             DERROR("add error:%d, key:%s, value:%d\n", ret, key, i);
         }
-        hashtable_print(ht, key);
+        table_print(tb, key);
         if (i < 102) {
             assert(node->data->data_count == 5);
         }else if (i >= 102 && i < 107) {
@@ -119,11 +129,11 @@ int main()
 
     for (i = 200; i < 213; i++) {
         sprintf(value, "%010d", i);
-        ret = hashtable_add_attr_bin(ht, key, value, attr, -1);
+        ret = hashtable_insert_binattr(ht, tbname, key, value, attr, -1);
         if (ret != MEMLINK_OK) {
             DERROR("add error:%d, key:%s, value:%d\n", ret, key, i);
         }
-        hashtable_print(ht, key);
+        table_print(tb, key);
 
         if (i == 200) {
             assert(node->data_tail->data_count == 1);
--- trunk/unittest/sortlist_test.c
+++ trunk/unittest/sortlist_test.c
@@ -4,18 +4,18 @@
 #include <stdio.h>
 #include <assert.h>
 
-int check_values(int *values, int values_count, HashNode *node)
+int check_values(int *values, int values_count, HashNode *node, Table *tb)
 {
     DINFO("<<<<<< check values, used:%d, value count:%d >>>>>>\n", node->used, values_count);
     assert(node->used == values_count); 
     DataBlock *dbk = node->data;
 
     int i, vi = 0;
-    int datalen = node->valuesize + node->attrsize;
+    int datalen = tb->valuesize + tb->attrsize;
     while (dbk) {
         char *itemdata = dbk->data;
         for (i = 0; i < dbk->data_count; i++) {
-            if (dataitem_have_data(node, itemdata, MEMLINK_VALUE_VISIBLE)){
+            if (dataitem_have_data(tb, node, itemdata, MEMLINK_VALUE_VISIBLE)){
                 DINFO("value cmp: %d, %d\n", values[vi], *(int*)itemdata);
                 assert(values[vi] == *(int*)itemdata);
                 vi++;
@@ -27,16 +27,17 @@ int check_values(int *values, int values_count, HashNode *node)
     return 0;
 }
 
-int add_one_value(HashTable *ht, HashNode *node, char *key, int value, char *attr, 
+int add_one_value(HashTable *ht, HashNode *node, char *name, char *key, int value, char *attr, 
                     int *values, int *values_count)
 {
     DINFO("add one: %d\n", value);
     //hashtable_print(ht, key);
     //check_values(values, *values_count, node);
     assert(node->used == *values_count);
-   
+    
+    Table *tb = hashtable_find_table(ht, name);
     //int value = 5;
-    int ret = hashtable_sortlist_add_attr_bin(ht, key, &value, attr);
+    int ret = hashtable_sortlist_insert_binattr(ht, name, key, &value, attr);
     if (ret != MEMLINK_OK) {
         DERROR("add error:%d, key:%s, value:%d\n", ret, key, value);
     }
@@ -48,9 +49,9 @@ int add_one_value(HashTable *ht, HashNode *node, char *key, int value, char *att
         DINFO("i:%d, value:%d\n", i, values[i]);
     }*/
     DINFO("hashtable print %s ...\n", key);
-    hashtable_print(ht, key);
+    table_print(tb, key);
     DINFO("check values ...\n");
-    check_values(values, *values_count, node);
+    check_values(values, *values_count, node, tb);
 
     return 0;
 }
@@ -58,7 +59,8 @@ int add_one_value(HashTable *ht, HashNode *node, char *key, int value, char *att
 int main()
 {
 #ifdef DEBUG
-	logfile_create("stdout", 3);
+	logfile_create("test.log", 3);
+	//logfile_create("stdout", 4);
 #endif
     HashTable *ht;
 
@@ -71,44 +73,48 @@ int main()
     int  attrnum = 0; 
     int  i = 0, ret;
     unsigned int attrformat[4] = {0};
+    char *name = "test";
 
     sprintf(key, "haha%03d", i);
-    hashtable_key_create_attr(ht, key, valuesize, attrformat, attrnum, 
+    hashtable_create_table(ht, name, valuesize, attrformat, attrnum, 
                               MEMLINK_SORTLIST, MEMLINK_VALUE_UINT4);
-   
+  
+    Table *tb = hashtable_find_table(ht, name);
     char attr[10] = {0x01};
     int value;
     int values[1024] = {0};
     int values_count = 0;
-    HashNode *node = hashtable_find(ht, key);
+    HashNode *node = NULL; // = table_find(tb, key);
 
     for (i = 10; i < 20; i++) {
         value = i * 2;
-        ret = hashtable_sortlist_add_attr_bin(ht, key, &value, attr);
+        ret = hashtable_sortlist_insert_binattr(ht, name, key, &value, attr);
         if (ret != MEMLINK_OK) {
             DERROR("add error:%d, key:%s, value:%d\n", ret, key, i);
         }
+        if (node == NULL)
+            node = table_find(tb, key);
         values[values_count] = value;
         values_count++;
         assert(node->used == values_count);
 
-        hashtable_print(ht, key);
+        table_print(tb, key);
     }
     qsort(values, values_count, sizeof(int), compare_int);
     /*for (i = 0; i < values_count; i++) {
         DINFO("i:%d, value:%d\n", i, values[i]);
     }*/
     //hashtable_print(ht, key);
-    check_values(values, values_count, node);
+    check_values(values, values_count, node, tb);
     assert(node->used == 10);
    
     value = 100;
     DINFO("add value:%d\n", value);
-    add_one_value(ht, node, key, value, attr, values, &values_count);
+    add_one_value(ht, node, name, key, value, attr, values, &values_count);
 
     value = 1;
     DINFO("add value:%d\n", value);
-    add_one_value(ht, node, key, value, attr, values, &values_count);
+    add_one_value(ht, node, name, key, value, attr, values, &values_count);
 
     return 0;
 }
